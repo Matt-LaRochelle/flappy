78f3ffdabe6dd049d5b991697846227363b6ed48
matthew <larochelle.matthew@gmail.com>
2023-04-14 10:50:38 -0700

    second commit?

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const BIRD_SIZE = 20;
+const GAME_WIDTH = 500;
+const GAME_HEIGHT = 500;
+const GRAVITY = 6;
+const JUMP_HEIGHT = 100;
+
+
+
+
+
+
+
 
 function App() {
+  const [birdPosition, setBirdPosition] = useState(250);
+
+  useEffect(() => {
+    let timeId;
+    if (birdPosition <  GAME_HEIGHT - BIRD_SIZE) {
+      timeId = setInterval(() => {
+        setBirdPosition((birdPosition) => birdPosition + GRAVITY);
+      }, 24);
+    }
+
+    return () => {
+      clearInterval(timeId);
+    };
+  });
+
+  const handleClick = () => {
+    let newBirdPosition = birdPosition - JUMP_HEIGHT;
+    setBirdPosition(newBirdPosition);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Div onClick={handleClick}>
+      <GameBox height={GAME_HEIGHT} width={GAME_WIDTH}>
+        <Bird size={BIRD_SIZE} top={birdPosition} />
+      </GameBox>
+    </Div>
   );
 }
 
 export default App;
+
+
+
+
+const Bird = styled.div`
+position: absolute;
+background-color: red;
+height: ${(props) => props.size}px;
+width: ${(props) => props.size}px;
+top: ${(props) => props.top}px;
+border-radius: 50%;
+`;
+
+const Div = styled.div`
+display: flex;
+width: 100%;
+justify-content: center;
+`;
+
+const GameBox = styled.div`
+height: ${(props) => props.height}px;
+width: ${(props) => props.height}px;
+background-color: blue;
+`;
